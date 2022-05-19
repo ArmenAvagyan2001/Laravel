13aefff5abfe231c2164db0db58ad91b297295ae
Karen Harutyunyan <k.h.a.200077@gmail.com>
2022-05-19 19:56:25 +0400

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,10 +33,10 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        //
-    }
+//    public function store(Request $request)
+//    {
+//        //
+//    }
 
     /**
      * Display the specified resource.
